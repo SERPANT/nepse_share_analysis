74e055359bf9959a54e15ac6e75b0974fcdf5a3d
Shreejit Rajbanshi <shreejit955@gmail.com>
2021-08-19 16:18:15 +0545

    update file

--- scraper-daemon/src/Share_scrapy/Share_scrapy/pipelines/share_scrapper_nepse/insert_new_share.py
+++ scraper-daemon/src/Share_scrapy/Share_scrapy/pipelines/share_scrapper_nepse/insert_new_share.py
@@ -32,22 +32,22 @@ class InsertNewShare:
             share_category_data = share_category_services.fetch_by_name(share_category)
 
             if share_category_data == None:
-                # return item
-                share_category_services.create([{
-                    "name": share_category
-                }])
+                return item
+                # share_category_services.create([{
+                #     "name": share_category
+                # }])
 
-                raise DropItem("Category created")
+                # raise DropItem("Category created")
 
 
-            # share_services.create_new_share_record([{
-            #     "name": share_name,
-            #     "symbol": share_symbol,
-            #     "shareCategoryId": share_category_data["id"],
-            #     "share_number": item["share_number"]
-            # }])
+            share_services.create_new_share_record([{
+                "name": share_name,
+                "symbol": share_symbol,
+                "shareCategoryId": share_category_data["id"],
+                "share_number": item["share_number"]
+            }])
 
-            # raise DropItem("Share created")
+            raise DropItem("Share created")
 
         if(share_record["share_number_nepse"] == None):
             share_services.patch_share_record(
