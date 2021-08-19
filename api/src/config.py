7e19745b9867b1d2c2409c48a1809752a05d37c3
Shreejit Rajbanshi <shreejit955@gmail.com>
2021-08-19 14:57:20 +0545

    update config

--- api/src/config.py
+++ api/src/config.py
@@ -1,15 +1,25 @@
 from box import Box
 from dotenv import dotenv_values
 
-env = dotenv_values()
+db = {}
+env = Box(dotenv_values())
 
-db = Box({
-    "DB_HOST": env["DB_HOST"],
-    "DB_PORT": env["DB_PORT"],
-    "DB_NAME": env["DB_NAME"],
-    "DB_PASSWORD": env["DB_PASSWORD"],
-    "DB_USER": env["DB_USER"]
-})
+if env.env == "prod":
+    db = Box({
+        "DB_HOST": env.DB_HOST_PROD,
+        "DB_PORT": env.DB_PORT_PROD,
+        "DB_NAME": env.DB_NAME_PROD,
+        "DB_PASSWORD": env.DB_PASSWORD_PROD,
+        "DB_USER": env.DB_USER_PROD
+    })
+else:
+     db = Box({
+        "DB_HOST": env.DB_HOST,
+        "DB_PORT": env.DB_PORT,
+        "DB_NAME": env.DB_NAME,
+        "DB_PASSWORD": env.DB_PASSWORD,
+        "DB_USER": env.DB_USER
+    })
 
 CONFIG = Box({
     "db": db
