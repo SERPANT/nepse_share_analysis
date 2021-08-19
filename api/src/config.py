e48654242a07fcad41af0cfb7ebe6b8bd324976e
Shreejit Rajbanshi <shreejit955@gmail.com>
2021-08-19 14:33:01 +0545

    Set up config for api

---
+++ api/src/config.py
@@ -0,0 +1,16 @@
+from box import Box
+from dotenv import dotenv_values
+
+env = dotenv_values()
+
+db = Box({
+    "DB_HOST": env["DB_HOST"],
+    "DB_PORT": env["DB_PORT"],
+    "DB_NAME": env["DB_NAME"],
+    "DB_PASSWORD": env["DB_PASSWORD"],
+    "DB_USER": env["DB_USER"]
+})
+
+CONFIG = Box({
+    "db": db
+})
--- api/src/db.py
+++ api/src/db.py
@@ -1,13 +1,16 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
+from config import CONFIG
+
 from models.base_model import Base
 
 class Db_Connection():
 
     def __init__(self):
         try:
-            connection_string = 'postgresql://postgres:root@localhost:5432/share_info'
+            connection_string = f"postgresql://{CONFIG.db.DB_USER}:{CONFIG.db.DB_PASSWORD}@{CONFIG.db.DB_HOST}:{CONFIG.db.DB_PORT}/{CONFIG.db.DB_NAME}"
+
             self.engine = sqlalchemy.create_engine(connection_string, echo=False,  pool_size=20, max_overflow=8)
             self.session_maker = sessionmaker(bind=self.engine) # used to create session
             print("Connection good")
